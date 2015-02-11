41f3b9dd1c2d9ba9c54f1bdf39c450e48c53e122
jjunior776 <juniorn@seznam.cz>
2015-02-11 17:22:22 +0100

    rozdelana prace na odstrel micku

---
+++ Assets/vystrel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vystrel : MonoBehaviour {
+	public Transform mistoVystrelu;
+	public LayerMask kulicka;
+	float radius = 1f;
+	bool pripravenKVystrelu=false;
+
+
+	// Use this for initialization
+	void FixedUpdate () {
+		pripravenKVystrelu = Physics2D.OverlapCircle (mistoVystrelu.position, radius,  kulicka);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((pripravenKVystrelu == true)&&(Input.GetKeyUp(KeyCode.Space))) {
+			Debug.Log("vystreleno");
+
+		}
+	}
+}
