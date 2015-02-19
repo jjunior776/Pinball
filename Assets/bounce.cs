c4b0672c14a5dec27ae7c0b4d2d86601cfadd08c
jjunior776 <juniorn@seznam.cz>
2015-02-19 15:35:26 +0100

    dodelany bouncery i s animaci

--- Assets/bounce.cs
+++ Assets/bounce.cs
@@ -4,20 +4,25 @@ using System.Collections;
 public class bounce : MonoBehaviour {
 	bool bounced = false;
 	public SliderJoint2D guma;
-	// Use this for initialization
+	JointMotor2D j = new JointMotor2D();
+
 	void OnTriggerEnter2D(Collider2D micek){
 		if (micek.tag == "micek") { 
 			if (bounced == false) {
-
+				j.motorSpeed = -500;
+				j.maxMotorTorque = 200;
 				bounced = true;
 			}
 		}
-
+		guma.motor = j;
 	}
 
 	void OnTriggerExit2D(Collider2D micek){
 		if (micek.tag == "micek") {
+			j.motorSpeed = 100;
+			j.maxMotorTorque = 200;
 			bounced = false;
 		}
+		guma.motor = j;
 	}
 }
