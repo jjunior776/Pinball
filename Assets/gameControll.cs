288279a7d5a4ca3b50f4385cff0d75ddc955c7be
jjunior776 <juniorn@seznam.cz>
2015-04-08 14:05:51 +0200

    konec hry progress

--- Assets/gameControll.cs
+++ Assets/gameControll.cs
@@ -2,26 +2,30 @@
 using System.Collections;
 
 public class gameControll : MonoBehaviour {
-	public static int score = 0;
-	public static int balls = 3;
+	public static int score;
+	public static int balls;
+
 	int sizeX= 100;
 	int sizeY= 25;
 	public int positionY=5;
 	void OnGUI () {
 		GUI.Box (new Rect (0, 0, sizeX, sizeY), "Body: "+score);
 		GUI.Box (new Rect (0,(sizeY+3),sizeX,sizeY), "Míčky: "+ balls);
+
+		if (balls <= 0) {
+			Debug.Log("prohral jsi");
+			
+		}
+
 	}
 
 	void Start () {
 		score = 0;
-		balls = 3;
+		balls = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (balls <= 0) {
-		
-		
-		}
+
 	}
 }
